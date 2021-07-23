b82b4801ad016f4d2e743182bf4b008767167c4f
Matthew R. Becker <becker.mr@gmail.com>
2021-07-23 11:00:43 -0500

    ENH do the thing

--- recipe/build_product.sh
+++ recipe/build_product.sh
@@ -15,4 +15,6 @@ else
 fi
 ln -s ${eigendir}/include/eigen3/Eigen ${PREFIX}/include/Eigen
 
-stackvana-build ${eups_product}
+echo ${eups_product}
+stackvana-build meas_extensions_shapeHSM
+# stackvana-build ${eups_product}
