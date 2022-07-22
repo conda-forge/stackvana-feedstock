d4cd0be6be088c707cc5d6d100fbde47623e9b98
Matthew R. Becker <beckermr@users.noreply.github.com>
2022-07-22 05:44:25 -0500

    Update build_product.sh

--- recipe/build_product.sh
+++ recipe/build_product.sh
@@ -1,29 +1,8 @@
-# conda uninstall eigen --force --yes --offline
-# mamba install eigen=3.3 --force --yes --no-deps
-
 set -ex
 
 export EUPSPKG_NJOBS=2
 
-# ###############################################################################
-# # now build eigen and symlink it to where it can be found by default
-# # this is for scarlet
-# # this can be removed once eigen 3.4 is in use
-# echo "
-# Building eigen and making the symlinks..."
-# stackvana-build eigen
-
-# echo "======================================"
-# echo "======================================"
-# ls -lah ${EUPS_PATH}/*/eigen/*
-# echo "======================================"
-# echo "======================================"
-
-# if [[ `uname -s` == "Darwin" ]]; then
-#     eigendir=$(compgen -G "${EUPS_PATH}/DarwinX86/eigen/g04a8d4365e*")
-# else
-#     eigendir=$(compgen -G "${EUPS_PATH}/Linux64/eigen/g04a8d4365e*")
-# fi
-# ln -s ${eigendir}/include/eigen3/Eigen ${PREFIX}/include/Eigen
+# needed by scarlet apparently
+ln -s ${PREFIX}/include/eigen3/Eigen ${PREFIX}/include/Eigen
 
 stackvana-build ${eups_product}
