9af85ca9045d35e5ee80bd227aff2e292f2bd3f6
jaimergp <jaimergp@users.noreply.github.com>
2023-11-17 15:48:40 +0100

    debug a bit, mock build

--- recipe/build_product.sh
+++ recipe/build_product.sh
@@ -1,5 +1,7 @@
 set -ex
-
+mkdir -p $PREFIX/bin
+echo "Some mention of $PREFIX" >> $PREFIX/bin/stakvana-product
+exit 0
 export EUPSPKG_NJOBS=2
 
 # needed by scarlet apparently
--- recipe/build_stackvana.sh
+++ recipe/build_stackvana.sh
@@ -1,6 +1,7 @@
-
+exit 0
 # get the (de)activate scripts
 for CHANGE in "activate" "deactivate"; do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    # cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    echo "$CHANGE and some mention of $PREFIX" > "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
