25828ac68f341f1d86a5064c5f0fe1d1938a4c18
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-02-04 16:58:42 -0600

    Update build_product.sh

--- recipe/build_product.sh
+++ recipe/build_product.sh
@@ -5,4 +5,4 @@ export EUPSPKG_NJOBS=2
 # needed by scarlet apparently
 ln -s ${PREFIX}/include/eigen3/Eigen ${PREFIX}/include/Eigen
 
-stackvana-build source_injection ${eups_product} 
+. ${RECIPE_DIR}/stackvana-build-local.sh source_injection ${eups_product} 
