f751ac532ee8142e70807164f38fc14d78d472b9
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-07-22 05:50:19 -0500

    test: try new eups

--- recipe/build_product.sh
+++ recipe/build_product.sh
@@ -5,4 +5,8 @@ export EUPSPKG_NJOBS=2
 # needed by scarlet apparently
 ln -s ${PREFIX}/include/eigen3/Eigen ${PREFIX}/include/Eigen
 
+# try newer eups
+conda uninstall eups --force --yes
+pip install --no-deps --no-build-isolation git+https://github.com/RobertLuptonTheGood/eups.git@u/timj/fix-close-file
+
 stackvana-build ${eups_product}
